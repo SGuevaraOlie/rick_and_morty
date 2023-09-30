e3a47a961ce5238affd6a105d7977ff8803f2892
SGuevaraOlie <santiguevara98@gmail.com>
2023-09-30 00:21:55 -0300

    30/9 webserver

---
+++ Client/src/App.jsx
@@ -0,0 +1,93 @@
+// Styles
+import './App.css';
+// Hooks
+import { useState, useEffect } from "react";
+import { Routes, Route, useLocation, useNavigate } from "react-router-dom";
+import axios from 'axios';
+// Components
+import Nav from './components/Nav/Nav';
+import Form from './components/Form/Form';
+import Cards from './components/Cards/Cards.jsx';
+import Favorites from './components/Favorites/Favorites';
+// Views
+import Detail from './Views/Detail/Detail.view';
+import About from './Views/About/About.view.jsx'
+import Error from './Views/Error/Error';
+import Home from './Views/Home/Home.view';
+// Helpers
+import PATHROUTES from './helpers/PathRoutes.helper';
+
+
+
+function App() {
+
+   const {pathname} = useLocation()
+
+   // Cards state
+   const [characters, setCharacters] = useState([])
+   const navigate = useNavigate();
+
+   // Login
+   const [access, setAccess] = useState(false);
+   const EMAIL = 'santiguevara98@gmail.com';
+   const PASSWORD = 'proyecto1';
+
+   const login = (userData) => {
+      if (userData.password === PASSWORD && userData.email === EMAIL){
+         setAccess(true);
+         navigate(PATHROUTES.HOME);
+      }
+   }
+   useEffect(() => {
+      !access && navigate('/');
+   }, [access]);
+
+   const logout = () => {
+      setAccess(false);
+      navigate(PATHROUTES.LOGIN);
+   }
+   
+   const onSearch = (id) => {
+      if (isNaN(id) || id <= 0 || id > 5) {
+         window.alert('¡No hay personajes con este ID!')
+      }
+      axios(`http://localhost:3001/rickandmorty/character/${id}`).then(({ data }) => {
+         if (data.name) {
+            if (!characters.find((char) => char.id === data.id)){
+            setCharacters((oldChars) => [...oldChars, data]);
+            } else {
+               window.alert('Este personaje ya está en la lista.');
+            }
+      }});
+   }
+
+   const onRandomSearch = () => {
+      const randomId = Math.floor(Math.random()*5)
+      onSearch(randomId);
+   }
+
+   const onClose = (id) => {
+      setCharacters(
+         characters.filter((char) => {
+            return char.id !== Number(id)
+         })
+      )
+   }
+
+   return (
+      <div className='App'>
+         {pathname !==  '/' && <Nav onSearch={onSearch} onRandomSearch={onRandomSearch} onLogOut={logout}/>}
+         <Routes>
+            <Route path={PATHROUTES.LOGIN} element={<Form login={login} />} />
+            <Route path={PATHROUTES.HOME} element={<Home />} />
+            <Route path={PATHROUTES.BROWSER} element={<Cards characters={characters} onClose={onClose}/>}/>
+            <Route path={PATHROUTES.ABOUT} element={<About />} />
+            <Route path={PATHROUTES.DETAIL} element={<Detail />} />
+            <Route path={PATHROUTES.FAVORITES} element={<Favorites />} />
+            <Route path='x' element={<Error />} />
+         </Routes>
+      </div>
+   );
+}
+
+export default App;
---
+++ Client/src/Views/Detail/Detail.view.jsx
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from 'react'
+import axios from 'axios';
+import styles from './Detail.module.css'
+import { useParams } from 'react-router-dom';
+
+const Detail = () => {
+    
+  const {id} = useParams()
+
+  const [character, setCharacter] = useState({})
+
+  useEffect(() => {
+    axios(`http://localhost:3001/rickandmorty/character/${id}`).then(({ data }) => {
+       if (data.name) {
+          setCharacter(data);
+       } else {
+          window.alert('No hay personajes con ese ID');
+       }
+    });
+    return setCharacter({});
+ }, [id]);
+
+  return (
+    <div className={styles.divDetail}>
+      <div className={styles.dataBox}>
+        <h2 className={styles.dataName}>{character?.name}</h2>
+        <h2 className={styles.data}>Status: {character?.status}</h2>
+        <h2 className={styles.data}>Specie: {character?.species}</h2>
+        <h2 className={styles.data}>Gender: {character?.gender}</h2>
+        <h2 className={styles.data}>Origin: {character.origin?.name}</h2>
+      </div>
+      <img src={character?.image} alt='' className={styles.img} />
+    </div>
+  )
+}
+
+export default Detail;
